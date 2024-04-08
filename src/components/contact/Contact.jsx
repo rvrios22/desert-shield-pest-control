5fa5ef7500bcf67fd3a7c406fd97f3343ebae096
Rafael Rios <rvrios22@gmail.com>
2024-04-08 15:38:41 -0700

    resolve: contact form issues

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,10 +1,25 @@
 import React, { useState, useRef } from "react";
 import emailjs from "@emailjs/browser";
+import Swal from "sweetalert2";
 import "./contact.css";
 
 function Contact() {
-  const [user, setUser] = useState({ name: "", email: "", message: "" });
+  const [user, setUser] = useState({ name: "", email: "", tel: '', message: "" });
   const form = useRef();
+
+  const toastSuccess = Swal.mixin({
+    toast: true,
+    position: "center",
+    showConfirmButton: false,
+    timer: 2000,
+    timerProgressBar: true,
+  });
+
+  const toastError = Swal.mixin({
+    toast: true,
+    position: "center",
+    showConfirmButton: true,
+  });
 
   const handleNameChange = (e) => {
     setUser({
@@ -16,6 +31,12 @@ function Contact() {
     setUser({
       ...user,
       email: e.target.value,
+    });
+  };
+  const handleTelChange = (e) => {
+    setUser({
+      ...user,
+      tel: e.target.value,
     });
   };
   const handleMessageChange = (e) => {
@@ -38,19 +59,43 @@ function Contact() {
         }
       )
       .then(
-        () => {
-          setUser({});
+        (res) => {
+          toastSuccess.fire({
+            icon: "success",
+            title: "Thanks for reaching out!",
+            text: "I'll respond to you as soon as I can.",
+            background: "#F5F6F7",
+            color: "black",
+          });
         },
         (error) => {
           console.log("FAILED...", error.text);
+          toastError.fire({
+            icon: "error",
+            title: "Oops",
+            text: "Looks like there's a bug in the system. Please reach out via phone while we resolve this issue.",
+            background: "#F5F6F7",
+            color: "black",
+          });
         }
-      );
+      )
+      .finally(() => {
+        setUser({
+          name: "",
+          email: "",
+          tel: '',
+          message: "",
+        });
+      });
   };
 
   return (
     <div id="contact" className="contact-container">
       <h2>Get In Touch</h2>
-      <p>We'd love to hear from you! Get in touch and we'll respond as quick as we can.</p>
+      <p>
+        We'd love to hear from you! Get in touch and we'll respond as quick as
+        we can.
+      </p>
       <form ref={form} onSubmit={sendEmail}>
         <label>Name</label>
         <input
@@ -67,6 +112,14 @@ function Contact() {
           value={user.email}
           onChange={handleEmailChange}
           placeholder="Please enter your email..."
+        />
+        <label>Phone</label>
+        <input
+          type="tel"
+          name="user_tel"
+          value={user.tel}
+          onChange={handleTelChange}
+          placeholder="Please enter your number..."
         />
         <label>Message</label>
         <textarea
